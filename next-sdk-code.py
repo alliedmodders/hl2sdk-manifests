c1e7cd6d54c3e9c5cc47d6f333987f861256901e
David Anderson <dvander@alliedmods.net>
2023-10-15 09:49:27 -0700

    Restyle with yapf --style pep8.

--- next-sdk-code.py
+++ next-sdk-code.py
@@ -2,6 +2,7 @@
 import os
 import json
 
+
 def main():
     top_code = 0
     for name in os.listdir('manifests'):
@@ -10,6 +11,7 @@ def main():
             obj = json.load(fp)
         top_code = max(top_code, obj['code'])
     print(top_code + 1)
+
 
 if __name__ == '__main__':
     main()
